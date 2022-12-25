f9ae77c0f2da4de6c8f657d5bac4f2bcd380dc05
GullianoC <119634631+GullianoC@users.noreply.github.com>
2022-12-24 23:08:13 -0300

    Revert "Primer update de nombres"
    This reverts commit 353e95a5d86bb393b73f2f80a795247033fa9eae.

--- Palindromos/src/Main.java
+++ Palindromos/src/Main.java
@@ -20,16 +20,6 @@ public class Main {
 
     public static void main (String[]args){
 
-        /*
-
-        El siguiente algoritmo se construye usando un test donde cualquier palabra o frase introducida en la pantalla se considera palindromo si es que escrita hacia adelante o hacia atrás se lee de la misma forma.
-        En particular, cuando se construyó este algoritmo, se tomó como referencia un test de internet donde se exigía que las siguientes palabras/frases fueran correctamente leídas
-        "Racecar!"
-        'A car, a man, a maraca.'
-        'Animal loots foliated detail of stool lamina.'
-
-        */
-
         System.out.println("Ahora vamos a chequear si la palabra o frase que vas a introducir es palindromo");
         System.out.println(" ");
         Palindromo();
